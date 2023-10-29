bf723d571fc4e58b9623d1bf20c19bf91e848b5f
x-io Technologies <social@x-io.co.uk>
2023-10-29 22:15:21 +0000

    Use latest x-io-PIC32-Library

--- Firmware/firmware/src/x-io-PIC32-Library/Timer/Timer.h
+++ Firmware/firmware/src/x-io-PIC32-Library/Timer/Timer.h
@@ -19,7 +19,7 @@
 /**
  * @brief Timer ticks per second.
  */
-#if defined __PIC32MX__
+#if (defined __PIC32MX__) || (defined __PIC32MM__)
 #define TIMER_TICKS_PER_SECOND (PERIPHERAL_BUS_CLOCK_FREQUENCY)
 #elif defined __PIC32MZ__
 #define TIMER_TICKS_PER_SECOND (PERIPHERAL_BUS_CLOCK_3_FREQUENCY)
@@ -27,13 +27,23 @@
 #error "Unsupported device."
 #endif
 
+/**
+ * @brief Timer ticks per millisecond.
+ */
+#define TIMER_TICKS_PER_MILLISECOND (TIMER_TICKS_PER_SECOND / 1000)
+
+/**
+ * @brief Timer ticks per microsecond.
+ */
+#define TIMER_TICKS_PER_MICROSECOND (TIMER_TICKS_PER_SECOND / 1000000)
+
 //------------------------------------------------------------------------------
 // Function declarations
 
-void TimerInitialise();
-void TimerDisable();
-uint32_t TimerGetTicks32();
-uint64_t TimerGetTicks64();
+void TimerInitialise(void);
+void TimerDeinitialise(void);
+uint32_t TimerGetTicks32(void);
+uint64_t TimerGetTicks64(void);
 void TimerDelayMilliseconds(const uint32_t milliseconds);
 void TimerDelayMicroseconds(const uint32_t microseconds);
 
